d42473f7bf2a7aea1f7e7c888a3ed4376faf73d5
Andrew Napoleone <Naps277@gmail.com>
2023-09-24 19:04:01 -0400

    eslint in foo.js

--- foo.js
+++ foo.js
@@ -1,24 +1,3 @@
-   /*
-   
-   whoa!    Hold your horses! This is badly formatted code.
-                       
-   
-*/
-
-
-
-       var message =      "Hello World"
-
-
-
-
-
-
-
-
-console.log(message)            // this should not show as an error
-
-
-
-
-
+/* whoa!    Hold your horses! This is badly formatted code. */
+const message = 'Hello World';
+console.log(message);// this should not show as an error
